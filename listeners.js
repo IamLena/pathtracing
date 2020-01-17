8f2adf172d7a6623ee298782dd972f73f53778f5
Lena <helly.luchina@gmail.com>
2020-01-17 15:22:40 +0300

    interface

--- listeners.js
+++ listeners.js
@@ -31,13 +31,16 @@ function startTracing(form) {
 
     let x, y, z
 
-    //camara position
+    //camera position
     x =  parseInt(params["CamPosx"])
     y =  parseInt(params["CamPosy"])
     z =  parseInt(params["CamPosz"])
     if (isNaN(x)) {x = 0}
-    if (isNaN(y)) {y = 30}
-    if (isNaN(z)) {z = 100}
+    if (isNaN(y)) {y = 10}
+    if (isNaN(z)) {z = 200}
+    params["CamPosx"] = x
+    params["CamPosy"] = y
+    params["CamPosz"] = z
     params["CamPos"] = new Vector3 (x, y, z)
     
     //camera angles
@@ -45,6 +48,37 @@ function startTracing(form) {
     let horz = parseInt(params["horz"])
     if (isNaN(vert)) {params["vert"] = 0} else {params["vert"] = vert}
     if (isNaN(horz)) {params["horz"] = 0} else {params["horz"] = horz}
+
+    //lenseposition
+    x =  parseInt(params["LenPosx"])
+    y =  parseInt(params["LenPosy"])
+    z =  parseInt(params["LenPosz"])
+    if (isNaN(x)) {x = 0}
+    if (isNaN(y)) {y = 10}
+    if (isNaN(z)) {z = 150}
+    params["LenPosx"] = x
+    params["LenPosy"] = y
+    params["LenPosz"] = z
+    params["LenPos"] = new Vector3 (x, y, z)
+
+    //lense raduis
+    let r1 = parseFloat(params["r1"])
+    if (isNaN(r1)) {params["r1"] = 10} else {params["r1"] = r1}
+    let r2 = parseFloat(params["r2"])
+    if (isNaN(r2)) {params["r2"] = 10} else {params["r2"] = r2}
+    
+    //lense width
+    let lenWid = parseFloat(params["lenWid"])
+    if (isNaN(lenWid) || lenWid <= 0) {params["lenWid"] = 20} else {params["lenWid"] = lenWid}
+    //lenseR
+    let lenseR = parseFloat(params["lenseR"])
+    if (isNaN(lenseR) || lenseR <= 0) {params["lenseR"] = 10} else {params["lenseR"] = lenseR}
+
+    // refractive indexes
+    let lenN = parseFloat(params["lenN"])
+    if (isNaN(lenN)) {params["lenN"] = 1.5} else {params["lenN"] = lenN}
+    let envN = parseFloat(params["envN"])
+    if (isNaN(envN)) {params["envN"] = 1} else {params["envN"] = envN}
 
     //obj radius
     let objr = parseInt(params["objr"])
@@ -55,57 +89,20 @@ function startTracing(form) {
     rgb = hexToRgb(params["objcol"])
     params["objcol"] = new Vector3 (rgb[0], rgb[1], rgb[2])
 
-    //lenseposition
-    x =  parseInt(params["LenPosx"])
-    y =  parseInt(params["LenPosy"])
-    z =  parseInt(params["LenPosz"])
-    if (isNaN(x)) {x = 0}
-    if (isNaN(y)) {y = 30}
-    if (isNaN(z)) {z = 100}
-    params["LenPos"] = new Vector3 (x, y, z)
-
-    //lense raduis
-    let r1 = parseFloat(params["r1"])
-    if (isNaN(r1)) {params["r1"] = 10} else {params["r1"] = r1}
-    let r2 = parseFloat(params["r2"])
-    if (isNaN(r2)) {params["r2"] = 10} else {params["r2"] = r2}
-    let lenseR = parseFloat(params["lenseR"])
-    if (isNaN(lenseR) || lenseR <= 0) {params["lenseR"] = 10} else {params["lenseR"] = lenseR}
-
-    //lense width
-    let lenWid = parseFloat(params["lenWid"])
-    if (isNaN(lenWid) || lenWid <= 0) {params["lenWid"] = 20} else {params["lenWid"] = lenWid}
-
-    // refractive indexes
-    let lenN = parseFloat(params["lenN"])
-    if (isNaN(lenN)) {params["lenN"] = 1.5} else {params["lenN"] = lenN}
-    let envN = parseFloat(params["envN"])
-    if (isNaN(envN)) {params["envN"] = 1} else {params["envN"] = envN}
-
-    //object positions
-    let objzStep = parseInt(params["objzStep"])
-    if (isNaN(objzStep)) {objzStep = 30}
-    params["objzStep"] = objzStep
-
+    //object position
     x =  parseInt(params["objPosx"])
     y =  parseInt(params["objPosy"])
     z =  parseInt(params["objPosz"])
     if (isNaN(x)) {x = 0}
-    if (isNaN(y)) {y = 30}
+    if (isNaN(y)) {y = 10}
     if (isNaN(z)) {z = 100}
     params["objPos"] = new Vector3 (x, y, z)
 
     let params2 = {}
-    let params3 = {}
-    let params4 = {}
     Object.assign(params2, params)
-    Object.assign(params3, params)
-    Object.assign(params4, params)
-    params2["objPos"] = new Vector3 (x, y, z + objzStep)
-    params3["objPos"] = new Vector3 (x, y, z + 2 * objzStep)
-    params4["objPos"] = new Vector3 (x, y, z + 3 * objzStep)
-
-    console.log(params)
+    let campos2 = new Vector3(params["LenPosx"] + params["CamPosz"] - params["LenPosz"], params["CamPosy"], params["LenPosz"])
+    params2["CamPos"] = campos2
+    params2["horz"] = params["horz"] - 90
 
     //VIEW 1
     const view1 = document.getElementById('view1')
@@ -116,49 +113,23 @@ function startTracing(form) {
     const tracer1 = new Tracer({scene: scene1, width: view1.width, height: view1.height, bounces: 10, debug: 1000})
     const renderer1 = new CanvasRenderer(tracer1, view1)
 
-    // // VIEW 2
-    // const view2 = document.getElementById('view2')
-    // const context2 = view2.getContext('2d')
-    // context2.clearRect(0, 0, view2.width, view2.height);
+    // VIEW 2
+    const view2 = document.getElementById('view2')
+    const context2 = view2.getContext('2d')
+    context2.clearRect(0, 0, view2.width, view2.height);
 
-    // const scene2 = new BoxScene(params2)
-    // const tracer2 = new Tracer({scene: scene2, width: view2.width, height: view2.height, bounces: 10, debug: 1000})
-    // const renderer2 = new CanvasRenderer(tracer2, view2)
-
-    // //VIEW 3
-    // const view3 = document.getElementById('view3')
-    // const context3 = view3.getContext('2d')
-    // context3.clearRect(0, 0, view3.width, view3.height);
-
-    // const scene3 = new BoxScene(params3)
-    // const tracer3 = new Tracer({scene: scene3, width: view3.width, height: view3.height, bounces: 10, debug: 1000})
-    // const renderer3 = new CanvasRenderer(tracer3, view3)
-
-    // //VIEW 4
-    // const view4 = document.getElementById('view4')
-    // const context4 = view4.getContext('2d')
-    // context4.clearRect(0, 0, view4.width, view4.height);
-
-    // const scene4 = new BoxScene(params4)
-    // const tracer4 = new Tracer({scene: scene4, width: view4.width, height: view4.height, bounces: 10, debug: 1000})
-    // const renderer4 = new CanvasRenderer(tracer4, view4)
+    const scene2 = new BoxScene(params2)
+    const tracer2 = new Tracer({scene: scene2, width: view2.width, height: view2.height, bounces: 10, debug: 1000})
+    const renderer2 = new CanvasRenderer(tracer2, view2)
 
     scene1.load().then(() => {
         tracer1.start();
     })
     
-    // scene2.load().then(() => {
-    //     tracer2.start();
-    // })
-    
-    // scene3.load().then(() => {
-    //     tracer3.start();
-    // })
+    scene2.load().then(() => {
+        tracer2.start();
+    })
 
-    // scene4.load().then(() => {
-    //     tracer4.start();
-    // })
-    
     interval = setInterval(timecount, 100)
 }
 
